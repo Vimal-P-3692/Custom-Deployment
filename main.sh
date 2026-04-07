86835e34dd1206610c6a3d2a00e45abcc77d7acd
Vimal-P-3692 <vimalp3692@gmail.com>
2026-04-07 21:40:30 +0530

    Added the service_utils.sh in the main file ...

--- main.sh
+++ main.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 set -e
-trap 'echo "ERROR at line $LINENO. Command: $BASH_COMMAND"; exit 1' ERR
+trap 'echo "❌ ERROR at line $LINENO. Command: $BASH_COMMAND"; exit 1' ERR
 
 source ./detect_pkg.sh
 source ./dotnet_utils.sh
 source ./system_utils.sh
-source ./build_utils.sh  
+source ./build_utils.sh
+source ./service_utils.sh 
 
 REPO_URL=$1
 DOTNET_VERSION=$2
-PROJECT_SUB_PATH=$3  
+PROJECT_SUB_PATH=$3
+SERVICE_NAME=$4      
+PORT=$5          
 
 BASE_DIR="$HOME"
 
@@ -32,12 +35,26 @@ fi
 # Step 4: Clone repo
 clone_repo "$REPO_URL" "$BASE_DIR"
 
+# Move into cloned repo first
+REPO_NAME=$(basename "$REPO_URL" .git)
+CLONED_PATH="$BASE_DIR/$REPO_NAME"
+
 # Step 5: Move into required sub-folder
-enter_project_path "$BASE_DIR" "$PROJECT_SUB_PATH"
+enter_project_path "$CLONED_PATH" "$PROJECT_SUB_PATH"
 
 # Step 6: Build steps
 dotnet_restore
 dotnet_build
 dotnet_publish
 
-echo "Deployment completed successfully!"
+# Step 7: Service setup
+PUBLISH_PATH="$(pwd)/publish"
+
+echo "Using publish path: $PUBLISH_PATH"
+
+create_service_file "$SERVICE_NAME" "$PORT" "$PUBLISH_PATH"
+reload_systemd
+start_service "$SERVICE_NAME"
+check_service_status "$SERVICE_NAME"
+
+echo "Deployment + Service setup completed successfully!"
